e3910bbbac64d6c7fdf4cc683922e8f7b7b3ff14
Ali <paymani@gmail.com>
2019-05-25 11:51:59 +0430

    -bugfix in scheduler tick comparison -added runLaterOnce -renaming members

--- src/uscheduler.cpp
+++ src/uscheduler.cpp
@@ -2,19 +2,19 @@
 
 uint32_t uScheduler::tick()
 {
-  return _tick;
+  return _interval;
 }
 void uScheduler::tick(uint32_t tick)
 {
-  _tick = tick;
+  _interval = tick;
 }
 
 uScheduler_check_t uScheduler::tickCheck()
 {
-  _tickDiff = _nowtick - _prevtick;
-  if (_tickDiff > _tick)
+  _tickDiff = _tickNow - _tickPrev;
+  if (_tickDiff >= _interval)
   {
-    _prevtick = _nowtick;
+    _tickPrev = _tickNow;
     if (_tickDiff < (1<<31))         //  check for overflow on millis between two consecutive runs
     {
       return(USCH_CHECK_RUN);
@@ -39,7 +39,7 @@ uScheduler_check_t uScheduler::semCheck()
 
 uScheduler_check_t uScheduler::checkRun()
 {
-  _nowtick = millis();    //  this should be run as fast as possible to get most accurate tick
+  _tickNow = millis();    //  this should be run as fast as possible to get most accurate tick
   if (tickCheck())
   {
     if (semCheck())
--- src/uscheduler.h
+++ src/uscheduler.h
@@ -12,34 +12,50 @@ typedef enum
 class uScheduler
 {
   private:
-    uint32_t _tick;         //  main interval
-    uint32_t _prevtick;     // previous tick in ms
-    uint32_t _nowtick;
+    uint32_t _interval;         //  main interval
+    uint32_t _tickPrev;     // previous tick in ms
+    uint32_t _tickNow;
     uint32_t _tickDiff;
     int _semaphore;
 
     uScheduler_check_t tickCheck();
     uScheduler_check_t semCheck();
     void (*_callback)(void);
+    void timerReset(){_tickPrev = millis();};    //  resets the internal timer
 
   public:
     uint32_t tick();
     void tick(uint32_t);
     uScheduler_check_t checkRun();
     uint32_t counter;
+    void runLaterOnce(int interval, void (*callback)(void))
+    {
+      _tickPrev = millis();
+      _semaphore = 1;
+      _interval = interval;
+      _callback = callback;
 
-    void semaphore(int semaphore)
-    {
-      _semaphore = semaphore;
     }
-    int semaphore()
+    void runLaterOnce(int interval)
     {
-      return _semaphore;
+      _tickPrev = millis();
+      _semaphore = 1;
+      _interval = interval;
     }
+    void runLaterOnce()
+    {
+      _tickPrev = millis();
+      _semaphore = 1;
+    }
+
+    void semaphore(int semaphore) { _semaphore = semaphore;}
+    int semaphore() {return _semaphore;}
+    uint32_t elapsed() {return millis() - _tickPrev;}
 
     uScheduler(uint32_t interval, void (*callback)(void), int semaphore = -1)
     {
-      _tick = interval;
+      _tickPrev = millis();         //  first run is calculated from the time of the setting
+      _interval = interval;
       _callback = callback;
       _semaphore = semaphore;
     }
